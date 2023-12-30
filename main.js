e4c08c790979516334c8bd14862013de8b0ee122
Shehroz Sadiq <125815507+Shehroz-Sadiq10@users.noreply.github.com>
2023-12-30 12:38:58 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "YouTuber", "Web Developer", "Android Developer", "iOS Developer"],
+    typespeed: 100,
+    backspeed: 100,
+    backdelay: 1000,
+    loop: true
+});
+;
